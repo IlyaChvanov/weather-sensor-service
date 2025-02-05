0510fe1b0a36e8d1f6483668331065d2d3e26fd1
ilya <ivchvanov@edu.hse.ru>
2025-02-05 17:00:26 +0300

    feat(root): add main class

---
+++ src/main/java/ru/hse/weathersensorservice/WeatherSensorServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.weathersensorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSensorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSensorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hse/weathersensorservice/WeatherSensorEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.weathersensorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSensorEntityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
